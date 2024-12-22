01eeb566f3a9789692b29710b05460818ce56333
Yarden Tenenboim <yardentenenboim@gmail.com>
2024-12-22 20:36:19 +0100

    Add `repeat` and `duration` methods to `Track`

--- src/track.rs
+++ src/track.rs
@@ -71,6 +71,93 @@ impl Track {
         self
     }
 
+    /// Repeats the Track `n` times.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use std::time::Duration;
+    ///
+    /// use proc_aud::prelude::*;
+    ///
+    /// let mut track = Track::default();
+    /// track.notes.push(
+    ///     Note {
+    ///         duration: Duration::from_secs(1),
+    ///         instrument: Sine { hz: 440.0.f() }.f() * Exp(0.5).f(),
+    ///         pan: 0.5.f(),
+    ///     }
+    ///     .into(),
+    /// );
+    /// assert_eq!(track.duration(), track.repeat(0).duration());
+    /// assert_eq!(track.repeat(2).duration(), Duration::from_secs(3));
+    /// ```
+    pub fn repeat(&mut self, n: usize) -> &mut Self {
+        let duration = self.duration();
+        let mut notes = self.notes.clone();
+        self.notes.reserve(n * self.notes.len());
+        for _ in 1..=n {
+            self.notes.extend(notes.iter_mut().map(|note| {
+                note.start += duration;
+                note.clone()
+            }));
+        }
+        self
+    }
+
+    /// Calculates the duration of the Track.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use std::time::Duration;
+    ///
+    /// use proc_aud::prelude::*;
+    ///
+    /// let mut track = Track::default();
+    ///
+    /// let instrument = Sine { hz: 440.0.f() }.f();
+    /// let pan = 0.5.f();
+    ///
+    /// track.notes.push( // 2 secs
+    ///     Note {
+    ///         duration: Duration::from_secs(2),
+    ///         instrument: instrument.clone(),
+    ///         pan: pan.clone(),
+    ///     }
+    ///     .start_at(Duration::ZERO),
+    /// );
+    /// track.notes.push( // 2 secs
+    ///     Note {
+    ///         duration: Duration::from_secs_f32(0.5),
+    ///         instrument: instrument.clone(),
+    ///         pan: pan.clone(),
+    ///     }
+    ///     .start_at(Duration::from_secs_f32(1.5)),
+    /// );
+    /// track.notes.push( // 2.5 secs
+    ///     Note {
+    ///         duration: Duration::from_secs_f32(1.5),
+    ///         instrument,
+    ///         pan,
+    ///     }
+    ///     .start_at(Duration::from_secs(1)),
+    /// );
+    ///
+    /// assert_eq!(track.duration(), Duration::from_secs_f32(2.5));
+    /// ```
+    pub fn duration(&self) -> Duration {
+        match self
+            .notes
+            .iter()
+            .map(|timed_note| timed_note.start + timed_note.note.duration)
+            .max()
+        {
+            Some(v) => v,
+            None => Duration::ZERO,
+        }
+    }
+
     pub fn render(&self, sample_rate: f32) -> RenderOutput {
         let mut output = RenderOutput {
             samples: vec![],
